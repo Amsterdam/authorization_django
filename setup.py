0435f7d5cc677156c2308d3a720a1d5f446f9e24
Evert Lammerts <evert@lucipher.net>
2017-03-28 13:56:19 +0200

    version bump for new release

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class PyTest(TestCommand):
 with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.2.5'
+version = '0.2.6'
 packages = ['authorization_django']
 requires = ['datapunt-authorization-levels', 'Django>=1.10.3', 'PyJWT>=1.4.2']
 requires_test = ['pytest==3.0.5', 'pytest-cov==2.4.0']
