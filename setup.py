6610e8ee6562c4aee16a5ddeba5bba1bdd7fbdf6
Pieter Stokkink <p.stokkink@amsterdam.nl>
2025-03-31 14:42:52 +0200

    bump version to v1.5.0

--- setup.py
+++ setup.py
@@ -1,43 +1,45 @@
-""" setuptools config
-"""
+"""setuptools config"""
+
 import sys
 
 from setuptools import setup
 
 
-with open('README.md', encoding='utf-8') as f:
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
-version = '1.4.0'
-packages = ['authorization_django']
-requires = ['Django>=1.10.3', 'requests>=2.20.1', 'jwcrypto>=1.4.2']
-requires_test = ['pytest>=3.0.5', 'pytest-cov>=2.4.0', 'requests_mock']
+version = "1.5.0"
+packages = ["authorization_django"]
+requires = ["Django>=1.10.3", "requests>=2.20.1", "jwcrypto>=1.4.2"]
+requires_test = ["pytest>=3.0.5", "pytest-cov>=2.4.0", "requests_mock"]
 requires_extras = {
-    'dev': [] + requires_test,
+    "dev": [] + requires_test,
+    "extended": ["djangorestframework>=3.15.2", "drf-spectacular>=0.28.0"]
+    + requires_test,
 }
 
 setup(
-    name='datapunt-authorization-django',
+    name="datapunt-authorization-django",
     version=version,
-    description='Datapunt authorization check for Django',
+    description="Datapunt authorization check for Django",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/Amsterdam/authorization_django',
-    author='Amsterdam Datapunt',
-    author_email='datapunt@amsterdam.nl',
-    license='Mozilla Public License Version 2.0',
+    long_description_content_type="text/markdown",
+    url="https://github.com/Amsterdam/authorization_django",
+    author="Amsterdam Datapunt",
+    author_email="datapunt@amsterdam.nl",
+    license="Mozilla Public License Version 2.0",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Topic :: System :: Systems Administration :: Authentication/Directory',
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     packages=packages,
     install_requires=requires,
