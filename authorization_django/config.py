96526db4e3034ebde199d406f6f0b07e48fb7882
Diederik van der Boor <vdboor@edoburu.nl>
2025-04-30 10:21:20 +0200

    Fix confusing error messages for invalid settings, by including the DATAPUNT_AUTHZ['...'] part.

--- authorization_django/config.py
+++ authorization_django/config.py
@@ -93,7 +93,9 @@ def load_settings():
 
 def _validate_values(user_settings: dict):
     if not user_settings["JWKS"] and not user_settings["JWKS_URL"]:
-        raise AuthzConfigurationError("Either JWKS or JWKS_URL must be set, or both")
+        raise AuthzConfigurationError(
+            f"Either {_settings_key}['JWKS'] or {_settings_key}['JWKS_URL'] must be set, or both"
+        )
 
     is_entra = (
         user_settings["JWKS_URL"]
@@ -105,21 +107,26 @@ def _validate_values(user_settings: dict):
         # As tokens handed out by Entra ID can come from other instances,
         # checking the issuer and audience is super important!
         raise AuthzConfigurationError(
-            "When using Microsoft Entra ID, make sure to set an 'iss' and 'aud' claim in CHECK_CLAIMS setting"
+            "When using Microsoft Entra ID, make sure to set an 'iss' and 'aud' claim"
+            f" in the {_settings_key}['CHECK_CLAIMS'] setting"
         )
 
     if isinstance(user_settings["MIN_SCOPE"], str):
         user_settings["MIN_SCOPE"] = (user_settings["MIN_SCOPE"],)
 
     if not isinstance(user_settings["FORCED_ANONYMOUS_ROUTES"], (list, tuple, set)):
-        raise AuthzConfigurationError("FORCED_ANONYMOUS_ROUTES must be a list, tuple or set")
+        raise AuthzConfigurationError(
+            f"{_settings_key}['FORCED_ANONYMOUS_ROUTES'] must be a list, tuple or set"
+        )
 
     if not isinstance(user_settings["PROTECTED"], (list, tuple, set)):
-        raise AuthzConfigurationError("PROTECTED must be a list, tuple or set")
+        raise AuthzConfigurationError(f"{_settings_key}['PROTECTED'] must be a list, tuple or set")
 
     for resource in user_settings["PROTECTED"]:
         if not isinstance(resource, tuple) or not len(resource) == 3:
-            raise ProtectedRecourceSyntaxError("Resource in PROTECTED must be a tuple of length 3")
+            raise ProtectedRecourceSyntaxError(
+                f"Resource in {_settings_key}['PROTECTED'] must be a tuple of length 3"
+            )
 
         (route, methods, scopes) = resource
         _validate_protected_route(route, user_settings)
@@ -130,35 +137,42 @@ def _validate_values(user_settings: dict):
 def _validate_protected_route(route, user_settings):
     """Validate 'route' in PROTECTED block."""
     if not isinstance(route, str):
-        raise AuthzConfigurationError("Route in PROTECTED resource must be a string")
+        raise AuthzConfigurationError(
+            f"Route in {_settings_key}['PROTECTED'] resource must be a string"
+        )
 
     for aroute in user_settings["FORCED_ANONYMOUS_ROUTES"]:
         if route.startswith(aroute):
             raise ProtectedRouteConflictError(
-                f"{route} is configured in PROTECTED, but this would be "
-                f"overruled by {aroute} in FORCED_ANONYMOUS_ROUTES"
+                f"{route} is configured in {_settings_key}['PROTECTED'], but this would be "
+                f"overruled by {aroute} in {_settings_key}['FORCED_ANONYMOUS_ROUTES']"
             )
 
 
 def _validate_protected_methods(methods):
     """Validate 'methods' in PROTECTED block."""
     if not isinstance(methods, list):
-        raise AuthzConfigurationError("Methods in PROTECTED resource must be a list")
+        raise AuthzConfigurationError(
+            f"Methods in {_settings_key}['PROTECTED'] resource must be a list"
+        )
 
     for method in methods:
         if method not in _methods_valid_options:
             str_methods = ", ".join(_methods_valid_options)
             raise AuthzConfigurationError(
-                f"Invalid value for methods: {method}. Must be one of {str_methods}."
+                f"Invalid value for methods: {method} in {_settings_key}['PROTECTED']."
+                f" Must be one of {str_methods}."
             )
 
 
 def _validate_protected_scopes(scopes, route):
     """Validate 'scopes' in PROTECTED block."""
     if not isinstance(scopes, list):
-        raise AuthzConfigurationError("Scopes in PROTECTED resource must be a list")
+        raise AuthzConfigurationError(
+            f"Scopes in {_settings_key}['PROTECTED'] resource must be a list"
+        )
 
     if not len(scopes) > 0:
         raise NoRequiredScopesError(
-            f"You must require at least one scope for protected route {route}"
+            f"You must require at least one scope for protected route {route} in {_settings_key}['PROTECTED']"
         )
