38cb7cbd7d9116957d922f1d5862dfd6c7261d87
Diederik van der Boor <vdboor@edoburu.nl>
2025-05-01 16:34:31 +0200

    Add a DRF permission class too (based on haal-centraal-proxy)

--- authorization_django/extensions/drf.py
+++ authorization_django/extensions/drf.py
@@ -3,7 +3,10 @@ import sys
 from django.conf import settings
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication
+from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
+
+from authorization_django.config import get_settings
 
 # Auto-register a schema extension when the project uses drf-spectacular
 if "drf_spectacular" in settings.INSTALLED_APPS or "drf_spectacular" in sys.modules:
@@ -11,7 +14,9 @@ if "drf_spectacular" in settings.INSTALLED_APPS or "drf_spectacular" in sys.modu
 
 
 class JWTAuthentication(BaseAuthentication):
-    """Bridge the JWT authentication from authorization-django to DRF views."""
+    """Bridge the JWT authentication from authorization-django to DRF views.
+    This can be used in the ``authentication_classes`` for APIView.
+    """
 
     www_authenticate_realm = "api"
 
@@ -31,3 +36,27 @@ class JWTAuthentication(BaseAuthentication):
 
     def authenticate_header(self, request: Request) -> str:
         return f'Bearer realm="{self.www_authenticate_realm}"'
+
+
+class HasTokenScopes(BasePermission):
+    """Permission check, wrapped in a DRF permissions adapter.
+    This can be used in the ``permission_classes`` for APIView.
+    """
+
+    message = "Required scopes not given in token."
+
+    def __init__(self, *needed_scopes):
+        self.needed_scopes = frozenset(needed_scopes or get_settings()["MIN_SCOPE"])
+
+    def has_permission(self, request, view):
+        """Check whether the user has all required scopes"""
+        # This essentially does what request.is_authorized_for() does, without the logging.
+        # In this scenario it's not clear whether this is the only permission check,
+        # so falsely logging that access is granted is a bit premature.
+        return get_settings()["ALWAYS_OK"] or set(request.get_token_scopes).issuperset(
+            self.needed_scopes
+        )
+
+    def has_object_permission(self, request, view, obj):
+        """Object-level permission is currently identical to view-level permission."""
+        return self.has_permission(request, view)
