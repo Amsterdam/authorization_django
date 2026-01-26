67300b7ab9c40635623ddc30ca5e27745cdd669b
lotte-amsterdam <lotte.van.der.velden@amsterdam.nl>
2026-01-26 14:30:17 +0100

    another small import fix

--- authorization_django/middleware.py
+++ authorization_django/middleware.py
@@ -7,7 +7,6 @@ import json
 import logging
 from time import time
 
-from django import http
 from django.http import HttpRequest
 from jwcrypto.common import JWException
 from jwcrypto.jwt import JWT, JWTExpired, JWTMissingKey
