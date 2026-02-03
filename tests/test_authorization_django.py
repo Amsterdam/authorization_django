550a8f8cfcc941c744ba9c9ac2b80832573b1eaf
JonaBenja <j.bosman@amsterdam.nl>
2026-02-03 15:46:36 +0100

    Allow both Entra and Keycloak to be configured at the same time

--- tests/test_authorization_django.py
+++ tests/test_authorization_django.py
@@ -13,7 +13,6 @@ from django.http import HttpResponse, JsonResponse
 from django.test import RequestFactory
 from jwcrypto.jwt import JWT
 from jwcrypto.common import JWException
-from jwcrypto.common import JWException
 
 from authorization_django import authorization_middleware, config, jwks
 from authorization_django.exceptions import AuthorizationError, InsufficientScopeError
